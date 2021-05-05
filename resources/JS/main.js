3b45034042f17df6757964b799c9848de2bca3bc
DjonDark <mrak.ivan@gmail.com>
2021-05-05 12:46:23 +0200

    main text shadow added, carousell done

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,3 +1,4 @@
+const slogan = document.querySelector('.slogan');
 const first = document.querySelector('.first');
 const second = document.querySelector('.second');
 const whiteout = document.querySelector('.whiteout');
@@ -12,8 +13,6 @@ window.addEventListener('load',()=>{
     }, 2500);
     setTimeout(() => {
         whiteout.classList.add('hide');
-    }, 4500);
-    setTimeout(() => {
         first.classList.add('white');
         second.classList.add('white');
     }, 4500);
@@ -23,6 +22,23 @@ window.addEventListener('load',()=>{
 })
 //
 
+//preload images to avoid flicker on first show
+function preloader() {
+	if (document.images) {
+
+		var img1 = new Image();
+		var img2 = new Image();
+		var img3 = new Image();
+		var img4 = new Image();
+
+		img1.src = "resources/images/curanakrevetu.jpg";
+		img2.src = "resources/images/dorucak.jpg";
+		img3.src = "resources/images/wellness2.jpg";
+		img4.src = "resources/images/terasa.jpg";
+	}
+}
+preloader();
+
 //main image carousell
 const one = document.querySelector('.one');
 let i=0;
@@ -31,14 +47,15 @@ setTimeout(() => {
     i+=1;
     console.log(i);
     if(i===5){
-        i=0;
+        
     }
     switch (i) {
         case 1: 
             one.style.background = "url('resources/images/curanakrevetu.jpg') bottom/cover";
+            slogan.style.textShadow= "0 0 1rem rgba(70, 70, 70, 0.5)";
             break;
         case 2: 
-            one.style.background = "url('resources/images/dorucak.jpg') bottom/cover";
+            one.style.background = "url('resources/images/dorucak.jpg') bottom/cover";    
             break;
         case 3: 
             one.style.background = "url('resources/images/wellness2.jpg') bottom/cover";
@@ -48,6 +65,7 @@ setTimeout(() => {
             break;
         case 5: 
             one.style.background = "url('resources/images/zalazak2.jpg') bottom/cover";
+            i=0;
             break;
         default:
             break;
