36cdeb88432117507d264f93b9f5a487aae1c87d
DjonDark <mrak.ivan@gmail.com>
2021-07-08 20:20:56 -0300

    gallery functionality added

--- resources/JS/gallery.js
+++ resources/JS/gallery.js
@@ -1,14 +1,20 @@
-const items = document.querySelectorAll('.item');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 const fullsizeImg = document.querySelector('.fullsize-img');
 const previousBtn = document.querySelector('.previous');
 const nextBtn = document.querySelector('.next');
+const gallery = document.querySelector('.gallery');
 const galleryImage = document.querySelectorAll('.gallery-image');
 
-// Open modal
-items.forEach(item => {
+// index gallery
+for(let i=0; i<galleryImage.length; i++){
+    galleryImage[i].dataset.index = i;
+}
+
+// click on image
+galleryImage.forEach(item => {
     item.addEventListener('click',()=>{
+        // Open modal
         body.classList.add('scroll-disabled');
         modal.style.top = window.scrollY + 'px';
         modal.classList.remove('disable');
@@ -17,23 +23,37 @@ items.forEach(item => {
         }, 50);
 
         // show clicked image in fullsize
-        let clickedImage = item.firstElementChild;
-        fullsizeImg.src = clickedImage.src;
-
-        //return index of clicked image
-        // -------------------------------------------
-        for (var i = 0; i < galleryImage.length; i++)
-        {
-            (function(index){
-                galleryImage[i].onclick = function(){
-                    let clicked = index;
-                }    
-            })(i);
-        }
-        // -------------------------------------------
-
+        fullsizeImg.src = galleryImage[clickedImage].src;
+        n = 0;
     })
 });
+
+// get index of clicked image
+gallery.addEventListener('mouseup',(e)=>{
+    clickedImage = e.target.dataset.index;
+});
+
+// Previous Button
+previousBtn.addEventListener('click',()=>{
+    n=n+1;
+    currentIndex = clickedImage-n;
+    if(currentIndex<0){
+        n=n-1;
+        currentIndex=0;
+    }
+    fullsizeImg.src = galleryImage[currentIndex].src;
+})
+
+// Next Button
+nextBtn.addEventListener('click',()=>{
+    n=n-1;
+    currentIndex = clickedImage-n;
+    if(currentIndex>galleryImage.length-1){
+        n=n+1;
+        currentIndex=galleryImage.length-1;
+    }
+    fullsizeImg.src = galleryImage[currentIndex].src;
+})
 
 // Close modal
 closeModal.addEventListener('click',()=>{
@@ -43,12 +63,3 @@ closeModal.addEventListener('click',()=>{
         modal.classList.add('disable');
     }, 200);
 })
-
-// Previous / Next button functionality
-
-// Previous Button
-previousBtn.addEventListener('click',()=>{
-    fullsizeImg.src = galleryImage[clicked-1].src;
-})
-
-
