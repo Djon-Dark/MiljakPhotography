a83aa39066d7a17da1b9c798a1d2c60f84fca840
DjonDark <mrak.ivan@gmail.com>
2021-07-12 23:30:28 -0300

    ios items renamed to normal

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -9,109 +9,20 @@ const section = document.querySelectorAll('.section');
 const first = document.querySelector('.first');
 const second = document.querySelector('.second');
 const whiteout = document.querySelector('.whiteout');
-const ioscarousell = document.querySelector('.ioscarousell');
+const backgroundSlideshow = document.querySelector('.background-slideshow');
 const one = document.querySelector('.one');
 const two = document.querySelector('.two');
 const footer = document.querySelector('footer');
 
 //STATUS MESSAGES:
-console.log(' STATUS: Na iOS workaround srediti poziciju slika');
+console.log(' STATUS: na mobitelu carousell slike sjebane, treba ih izrezati i prilagoditi tim ekranima - IMAGESET');
 console.log(' STATUS: Na iPad KAKO MOGU POMOCI VAMA gap nije podrzan za flex, napravi fallback');
-console.log(' STATUS: Na iPad portrait mode logo zaljepljen za vrh, a linkovi budu na sredini navbara');
-console.log(' STATUS: Na iPad prvi image carousell se valjda loada u obje verzije?');
-/*
-//whiteout - load animation
-window.addEventListener('load',()=>{
-    //navbar.classList.add('disable');
-    setTimeout(() => {
-        first.classList.remove('hide');
-    }, 500);
-    setTimeout(() => {
-        second.classList.remove('hide');
-    }, 2500);
-    setTimeout(() => {
-        whiteout.classList.add('hide');
-        first.classList.add('white');
-        second.classList.add('white');
-    }, 4500);
-    setTimeout(() => {
-        whiteout.classList.add('disable');
-        body.classList.remove('scroll-disabled');
-    }, 7000);
-})
-//
 
-//preload images to avoid flicker on first show
- const preloader = function () {
-	if (document.images) {
-
-		var img1 = new Image();
-		var img2 = new Image();
-		var img3 = new Image();
-		var img4 = new Image();
-		var img5 = new Image();
-		var img6 = new Image();
-		var img7 = new Image();
-
-		img1.src = "resources/images/zalazak2.jpg";
-		img2.src = "resources/images/dorucak.jpg";
-		img3.src = "resources/images/wellness2.jpg";
-		img4.src = "resources/images/terasa.jpg";
-		img5.src = "resources/images/bazenhotel.jpg";
-		img6.src = "resources/images/plavikauc.jpg";
-		img7.src = "resources/images/soba1.jpg";
-	}
-}
-preloader();
-//
-*/
 let isApple = /iPhone|iPod/i.test(navigator.userAgent);
 
 //main image carousell
-const carousellBackground = ()=>{
-    let i=0;
-    setTimeout(() => {
-    setInterval(() => {
-        i+=1;
-        //console.log(i);
-        switch (i) {
-            case 1: 
-                one.style.background = "url('resources/images/curanakrevetu.jpg') bottom/cover";
-                break;
-            case 2: 
-                one.style.background = "url('resources/images/dorucak.jpg') bottom/cover";    
-                break;
-            case 3: 
-                one.style.background = "url('resources/images/wellness2.jpg') bottom/cover";
-                break;
-            case 4: 
-                one.style.background = "url('resources/images/terasa.jpg') bottom/cover";
-                break;
-            case 5: 
-                one.style.background = "url('resources/images/zalazak2.jpg') bottom/cover";
-                break;
-            case 6: 
-                one.style.background = "url('resources/images/bazenhotel.jpg') bottom/cover";
-                break;
-            case 7: 
-                one.style.background = "url('resources/images/plavikauc.jpg') bottom/cover";
-                break;
-            case 8: 
-                one.style.background = "url('resources/images/soba1.jpg') bottom/cover";
-                i=0;
-                break;
-            default:
-                break;
-        }
-    }, 5000); 
-    }, 3000)
-}
-if(!isApple){
-    carousellBackground();
-}
-//iOS main image carousell
 function slider (){
-    const slides = document.querySelectorAll('.iosbackground');
+    const slides = document.querySelectorAll('.background-slide');
     let i=0;
         setInterval(() => {
             slides[i].style.opacity = '1';
@@ -130,7 +41,6 @@ function slider (){
             }
         }, 5000);
 }
-if(isApple){
-    slider();
-    ioscarousell.style.display = 'block';
-}
+
+slider();
+
